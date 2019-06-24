a76b4d7f9de0c0aef38d76429e9cbbc83bf8025e
Codepirate4 <41866013+Codepirate4@users.noreply.github.com>
2019-06-24 18:45:22 +0530

    Py file for FaceDetection.

---
+++ CV_faceDetection.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('C:\\Users\\hp\\Desktop\\imageprocessing\\venv\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for(x,y,w,h )in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y:y + h, x:x + w]  # (ycord_start, ycord_end)
+        roi_color = frame[y:y + h, x:x + w]
+        img_item = "7.png"
+        cv2.imwrite(img_item, roi_color)
+
+        color = (255, 0, 0)  # BGR 0-255
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+
+
+    cv2.imshow('FRAME',frame)
+
+    k= cv2.waitKey(1)
+    if k & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
